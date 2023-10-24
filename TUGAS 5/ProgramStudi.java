11dd087a5cdd7ebfc056f7e82d45d20be9071825
Fitriya Mawadah Warohmah <146052469+fitriyamawadahw@users.noreply.github.com>
2023-10-24 18:04:39 +0700

    Add files via upload

---
+++ TUGAS 5/ProgramStudi.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihandb;
+
+/**
+ *
+ * @author Fitriyah
+ */
+public class ProgramStudi {
+
+    /**
+     * @return the kodeProdi
+     */
+    public String getKodeProdi() {
+        return kodeProdi;
+    }
+
+    /**
+     * @param kodeProdi the kodeProdi to set
+     */
+    public void setKodeProdi(String kodeProdi) {
+        this.kodeProdi = kodeProdi;
+    }
+
+    /**
+     * @return the namaProdi
+     */
+    public String getNamaProdi() {
+        return namaProdi;
+    }
+
+    /**
+     * @param namaProdi the namaProdi to set
+     */
+    public void setNamaProdi(String namaProdi) {
+        this.namaProdi = namaProdi;
+    }
+
+    /**
+     * @return the jenjang
+     */
+    public String getJenjang() {
+        return jenjang;
+    }
+
+    /**
+     * @param jenjang the jenjang to set
+     */
+    public void setJenjang(String jenjang) {
+        this.jenjang = jenjang;
+    }
+
+    /**
+     * @return the namaKaprodi
+     */
+    public String getNamaKaprodi() {
+        return namaKaprodi;
+    }
+
+    /**
+     * @param namaKaprodi the namaKaprodi to set
+     */
+    public void setNamaKaprodi(String namaKaprodi) {
+        this.namaKaprodi = namaKaprodi;
+    }
+
+private String kodeProdi;
+private String namaProdi;
+private String jenjang;
+private String namaKaprodi;
+    
+}
