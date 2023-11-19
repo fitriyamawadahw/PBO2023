3bc8c48e0f8c6a7767cdf885602cace5dd8c9a6c
Fitriya Mawadah Warohmah <146052469+fitriyamawadahw@users.noreply.github.com>
2023-11-19 21:20:21 +0700

    Add files via upload

---
+++ TUGAS 12/src/DataBukuPOJO.java
@@ -0,0 +1,137 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectuas;
+
+/**
+ *
+ * @author Fitriyah
+ */
+public class DataBukuPOJO {
+
+    /**
+     * @return the ISBN
+     */
+    public String getISBN() {
+        return ISBN;
+    }
+
+    /**
+     * @param ISBN the ISBN to set
+     */
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    /**
+     * @return the judul
+     */
+    public String getJudul() {
+        return judul;
+    }
+
+    /**
+     * @param judul the judul to set
+     */
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    /**
+     * @return the subJudul
+     */
+    public String getSubJudul() {
+        return subJudul;
+    }
+
+    /**
+     * @param subJudul the subJudul to set
+     */
+    public void setSubJudul(String subJudul) {
+        this.subJudul = subJudul;
+    }
+
+    /**
+     * @return the pengarang
+     */
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    /**
+     * @param pengarang the pengarang to set
+     */
+    public void setPengarang(String pengarang) {
+        this.pengarang = pengarang;
+    }
+
+    /**
+     * @return the penerbit
+     */
+    public String getPenerbit() {
+        return penerbit;
+    }
+
+    /**
+     * @param penerbit the penerbit to set
+     */
+    public void setPenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+
+    /**
+     * @return the tahunTerbit
+     */
+    public String getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    /**
+     * @param tahunTerbit the tahunTerbit to set
+     */
+    public void setTahunTerbit(String tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    /**
+     * @return the jumlahHalaman
+     */
+    public String getJumlahHalaman() {
+        return jumlahHalaman;
+    }
+
+    /**
+     * @param jumlahHalaman the jumlahHalaman to set
+     */
+    public void setJumlahHalaman(String jumlahHalaman) {
+        this.jumlahHalaman = jumlahHalaman;
+    }
+
+    /**
+     * @return the jumlahBuku
+     */
+    public String getJumlahBuku() {
+        return jumlahBuku;
+    }
+
+    /**
+     * @param jumlahBuku the jumlahBuku to set
+     */
+    public void setJumlahBuku(String jumlahBuku) {
+        this.jumlahBuku = jumlahBuku;
+    }
+
+
+private String ISBN;
+private String judul;
+private String subJudul;
+private String pengarang;
+private String penerbit;
+private String tahunTerbit;
+private String jumlahHalaman;
+private String jumlahBuku;
+
+
+    
+}
